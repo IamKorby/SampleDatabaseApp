10b741b6c96673083acfdbdfb0d4a47aad5f6239
IamKorby <justinkerrbie_96@yahoo.com>
2016-03-01 18:48:15 +0800

    Updated and removed some files.

--- SampleDatabaseApp/app/src/main/java/com/example/justin/notesdatabaseapp/NoteCursorAdapter.java
+++ SampleDatabaseApp/app/src/main/java/com/example/justin/notesdatabaseapp/NoteCursorAdapter.java
@@ -13,6 +13,18 @@ import android.widget.TextView;
  */
 public class NoteCursorAdapter extends CursorRecyclerViewAdapter<NoteCursorAdapter.NoteViewHolder>
 {
+    private OnItemClickListener mOnItemClickListener;
+
+    public void setmOnItemClickListener(OnItemClickListener m)
+    {
+        this.mOnItemClickListener = m;
+    }
+
+    public interface OnItemClickListener
+    {
+        public void onItemClick(int id);
+    }
+
     public NoteCursorAdapter(Context context, Cursor cursor)
     {
         super(context, cursor);
@@ -24,23 +36,43 @@ public class NoteCursorAdapter extends CursorRecyclerViewAdapter<NoteCursorAdapt
         String title = cursor.getString(cursor.getColumnIndex(Note.COLUMN_TITLE));
 
         viewHolder.tvTitle.setText(title);
+        viewHolder.container.setTag(cursor.getInt(cursor.getColumnIndex(Note.COLUMN_ID)));
+        viewHolder.container.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // call on item click here
+                int dbid = Integer.parseInt(v.getTag().toString());
+                mOnItemClickListener.onItemClick(dbid);
+            }
+        });
     }
 
     @Override
     public NoteViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
     {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, null);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
         return new NoteViewHolder(v);
     }
 
     public class NoteViewHolder extends RecyclerView.ViewHolder
     {
         TextView tvTitle;
+        View container;
 
         public NoteViewHolder(View itemView)
         {
             super(itemView);
             tvTitle = (TextView) itemView.findViewById(R.id.tv_title);
+            container = itemView.findViewById(R.id.container);
         }
     }
 }
+
+// SET UP LISTENER
+// 1. Create interface
+// 2. Create Local variable of interface
+// 3. Create a setter for variable
+// implement call
+// implement callback
--- SampleDatabaseApp/app/src/main/java/com/example/justin/notesdatabaseapp/ViewNoteActivity.java
+++ SampleDatabaseApp/app/src/main/java/com/example/justin/notesdatabaseapp/ViewNoteActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,10 +13,11 @@ public class ViewNoteActivity extends AppCompatActivity
 
     public final static int REQUEST_CODE_EDIT = 0;
 
-    private TextView tvTitle;
-    private TextView tvNote;
-    private ImageButton buttonEdit;
+    private EditText etTitle;
+    private EditText etNote;
     private ImageButton buttonDelete;
+    private ImageButton buttonSave;
+    private ImageButton buttonCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,10 +25,11 @@ public class ViewNoteActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_note);
 
-        tvTitle = (TextView) findViewById(R.id.tv_title);
-        tvNote = (TextView) findViewById(R.id.tv_note);
-        buttonEdit = (ImageButton) findViewById(R.id.button_edit);
+        etTitle = (EditText) findViewById(R.id.et_title);
+        etNote = (EditText) findViewById(R.id.et_note);
         buttonDelete = (ImageButton) findViewById(R.id.button_delete);
+        buttonSave = (ImageButton) findViewById(R.id.button_edit);
+        buttonCancel = (ImageButton) findViewById(R.id.button_cancel);
     }
 
 }
